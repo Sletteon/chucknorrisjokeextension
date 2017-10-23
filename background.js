7dee635f2b29ebb2dff6d0251e28b529f4bdd0fe
keksoskajoska <bszandras@gmail.com>
2017-10-23 16:44:59 +0200

    vicc 1.6.1

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	var joke = httpGet('https://api.chucknorris.io/jokes/random');
 	var parsedjoke = JSON.parse(joke)
 	var tenylegjoke = parsedjoke.value;
-    alert(tenylegjoke);
+    alert(tenylegjoke);                                     // joke :D
     console.log(parsedjoke)
     chrome.tabs.executeScript(tab.id, {file: "content_script.js"})
 });
