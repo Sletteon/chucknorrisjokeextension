d003ed105eb834b9f3f517c8b6fa026bc3401b1d
sletteon <misi.g126@gmail.com>
2017-10-23 16:04:28 +0200

    httprequest

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # viccextension
+TODO:
+	Andris:
+		Megjelenítés
+	Misi:
+		Stringslicing
+			(Amit az APItól visszakapunk...) 
+			pl.: {"joke":"Chuck Norris doesn\u0027t use mac.","category":"Chuck Norris","oneLiner":false}
+			link: http://webknox.com:8080/jokes/random?maxLength=30&apiKey=bfaihgfgifxulhbvojacbknnddaakmr
--- background.js
+++ background.js
@@ -1,4 +1,14 @@
+function httpGet(theUrl)
+{
+	    var xmlHttp = new XMLHttpRequest();
+	        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+		    xmlHttp.send( null );
+		        return xmlHttp.responseText;
+}
+
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-    alert("Boi Oh Boi");
+	var joke = httpGet('https://api.chucknorris.io/jokes/random')
+    alert(joke);
     chrome.tabs.executeScript(tab.id, {file: "content_script.js"})
 });
