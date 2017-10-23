701ae73a03eef85ef597d3fe1350f4f1c86451c0
keksoskajoska <bszandras@gmail.com>
2017-10-23 16:10:49 +0200

    vicc 1.3

--- background.js
+++ background.js
@@ -5,8 +5,6 @@ function httpGet(theUrl)
 		    xmlHttp.send( null );
 		        return xmlHttp.responseText;
 }
-
-
 chrome.browserAction.onClicked.addListener(function(tab) {
 	var joke = httpGet('https://api.chucknorris.io/jokes/random')
     alert(joke);
