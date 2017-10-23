95b8a30a96010f7f9db41d617efb88c20c8f9e44
sletteon <misi.g126@gmail.com>
2017-10-23 16:31:21 +0200

    JSON.parse a megoldás

--- background.js
+++ background.js
@@ -6,9 +6,11 @@ function httpGet(theUrl)
 		        return xmlHttp.responseText;
 }
 
-
 chrome.browserAction.onClicked.addListener(function(tab) {
-	var joke = httpGet('https://api.chucknorris.io/jokes/random')
-    alert(joke);
+	var joke = httpGet('https://api.chucknorris.io/jokes/random');
+	var parsedjoke = JSON.parse(joke)
+	var tenylegjoke = parsedjoke.value;
+    alert(tenylegjoke);
+    console.log(parsedjoke)
     chrome.tabs.executeScript(tab.id, {file: "content_script.js"})
 });
